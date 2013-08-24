34382b98bb4ff55119282186e6187430a2f84ab6
EPSIlLON <alexgreogirson@gmail.com>
2013-08-24 14:04:00 -0500

    Added .bat file

---
+++ sepia.bat
@@ -0,0 +1,69 @@
+cd /
+cd xampp
+cd htdocs
+mkdir halospot
+cd halospot
+mkdir assets
+mkdir system
+cd system
+type NUL > index.html
+cd ..
+mkdir app
+mkdir config
+cd config
+type NUL > index.html
+cd ..
+type NUL > .htaccess
+type NUL > index.php
+type NUL > readme
+cd assets
+type NUL > index.html
+mkdir img
+cd img
+type NUL > index.html
+cd ..
+mkdir js
+cd js
+type NUL > index.html
+cd ..
+mkdir style
+cd style
+type NUL > index.html
+cd ..
+mkdir uploads
+cd uploads
+type NUL > index.html
+cd ..
+mkdir static
+cd static
+type NUL > index.html
+cd ..
+cd ..
+cd system
+type NUL > index.html
+mkdir database_
+cd database_
+type NUL > index.html
+cd ..
+mkdir functions
+cd functions
+type NUL > index.html
+cd ..
+mkdir scripts
+cd scripts
+type NUL > index.html
+cd ..
+mkdir config
+cd config
+type NUL > index.html
+cd ..
+cd ..
+cd app
+mkdir config
+cd config
+type NUL > index.html
+cd ..
+mkdir src
+cd src
+type NUL > index.html
+cd ..
